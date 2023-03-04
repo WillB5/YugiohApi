9c9b33af71d6495695a8352b8194e7a2fcc9fb93
willb <willbarrera9@gmail.com>
2023-03-04 17:20:39 -0600

    Add project files.

---
+++ YugiohApi/Models/YugiohCard.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YugiohApi.Models
+{
+    public class YugiohCard
+    {
+        [Required]
+        public Guid Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public string? Description { get; set; } 
+        public CardType CardType { get; set; }
+
+    }
+
+    public enum CardType
+    {
+        Monster, Spell, Trap
+    }
+}
---
+++ YugiohApi/Models/YugiohContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace YugiohApi.Models
+{
+    public class YugiohContext : DbContext
+    {
+        public YugiohContext(DbContextOptions<YugiohContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<YugiohCard> YugiohCards { get; set; } = null!;
+    }
+}
